7fa85c2cddea31707836ac4d9752922dbd2805cb
kaderabdul79 <kaderabdul7980@gmail.com>
2023-09-05 06:19:51 +0600

    installed vuetify,vform and seyp done

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,7 @@
 <template>
-   <router-view />
+  <router-view />
   App.vue
+   <v-btn color="success">text</v-btn>
 </template>
 
 <script setup>
--- resources/js/main.js
+++ resources/js/main.js
@@ -2,11 +2,24 @@ import App from './App.vue'
 import router from './router'
 
 import { createApp } from 'vue'
-// import App from './App.vue'
-// import router from './router'
+import '@mdi/font/css/materialdesignicons.css'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+      defaultSet: 'mdi', 
+    },
+})
+
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
