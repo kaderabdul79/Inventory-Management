a44d140d73a918cb950a64b431a05a8b315856b4
kaderabdul79 <kaderabdul7980@gmail.com>
2023-09-07 06:19:51 +0600

    created model and define fillable property

---
+++ app/Models/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'category_id', 'size_id', 'brand_id', 'description', 'price', 'quantity_in_stock'];
+}
