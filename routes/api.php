0d31768f41b2ff1bcc998344c183502c45487725
kaderabdul79 <kaderabdul7980@gmail.com>
2023-09-24 06:19:51 +0600

    used everywhere snactum middleware, since i dont want to give the access without authentcation

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SizeController;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
@@ -22,9 +21,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 // users
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 // users
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
@@ -32,19 +31,23 @@ Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'getUser
 Route::middleware('auth:sanctum')->delete('/logout', [UserController::class, 'logout']);
 Route::get('/overview', [UserController::class, 'overview']);
 
-// Categories
-Route::resource('categories', CategoryController::class);
-Route::get('/active-categories', [CategoryController::class,'getActiveCategories']);
-// Sizes
-Route::resource('sizes', SizeController::class);
-Route::get('/active-sizes', [SizeController::class,'getActiveSizes']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Categories
+    Route::resource('categories', CategoryController::class);
+    Route::get('/active-categories', [CategoryController::class, 'getActiveCategories']);
 
-// Brands
-Route::resource('brands', BrandController::class);
-Route::get('/active-brands', [BrandController::class,'getActiveBrands']);
-// Products
-Route::resource('products', ProductController::class);
-Route::get('products/search', [ProductController::class,'search']);
+    // Sizes
+    Route::resource('sizes', SizeController::class);
+    Route::get('/active-sizes', [SizeController::class, 'getActiveSizes']);
 
-// stocks
-Route::resource('stocks', StockController::class);
+    // Brands
+    Route::resource('brands', BrandController::class);
+    Route::get('/active-brands', [BrandController::class, 'getActiveBrands']);
+
+    // Products
+    Route::resource('products', ProductController::class);
+    Route::get('products/search', [ProductController::class, 'search']);
+
+    // Stocks
+    Route::resource('stocks', StockController::class);
+});
